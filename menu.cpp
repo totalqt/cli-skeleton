99de3e504d691d954e0240d58357d568a10673a4
totalqt <admin@totalqt.com>
2022-05-09 16:49:49 -0700

    Initial commit for menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,66 @@
+#include "menu.h"
+#include <stdio.h>
+#include <iostream>
+
+Menu::Menu() {
+
+}
+
+Menu::Menu(std::string initHeader) {
+    
+    hasHeader = true;
+    header = initHeader;
+    
+}
+
+Menu::Menu(std::string initHeader, void (*initCallback)(int)) {
+    
+    hasHeader = true;
+    header = initHeader;
+    
+    globalCallbackPtr = initCallback;
+    
+}
+
+Menu::Menu(void (*initCallback)(int)) {
+    
+    globalCallbackPtr = initCallback;
+    
+}
+
+void Menu::addItem(std::string title) {
+    // Create menu item and add to list
+    menuItems.push_back(new MenuItem(title, count));
+    
+    // Increment count
+    count++;
+}
+
+void Menu::addItem(std::string title,void(*funcPtr)()) {
+    menuItems.push_back(new MenuItem(title, funcPtr, count));
+    count++;
+}
+
+
+void Menu::printMenu() {
+    
+    int option = 0;
+    
+    if (hasHeader) std::cout << header << "\n" << std::endl;
+    
+    for (int i = 0; i < menuItems.size(); i++) std::cout << i << ".) " << menuItems[i]->getTitle() << std::endl;
+
+    
+    std::cout << "\nSelect option: ";
+    std::cin >> option;
+    
+    if (option < 0 && option >= menuItems.size()) {
+        std::cout << "Not a valid selection.  Please try again." << std::endl;
+        printMenu();
+    } else {
+        menuItems[option]->performCallback();
+        
+        if (globalCallbackPtr != nullptr) globalCallbackPtr(option);
+    }
+    
+}
